0d77ab5aab74204873d4998d6d9dc06853c3bd62
Benjamin Ward <bdward@ucsd.edu>
2017-02-16 06:06:21 -0800

    finished lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -14,25 +14,63 @@ function initializePage() {
 	$('#colorBtn').click(randomizeColors);
 }
 
-/*
+/*id
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
+	
 	// Prevent following the link
 	e.preventDefault();
-
+    
 	// Get the div ID, e.g., "project3"
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
+	
 
 	console.log("User clicked on project " + idNumber);
+
+	
+	$.get("/project/" + idNumber , callback);
+
+	
+
+
+
 }
 
+function callback(result){
+	console.log(result);
+	var help = '<a href="#" class="thumbnail">' +
+	'<img src="' + result['image'] + '"class=detailsImage"></a>' +  
+	'<h5>' + result['date'] + '</h5>' + '<p>' + result['summary'] + '</p>';
+	$(".details#project"+ result['id']).html(help);
+	
+
+}
+    
+	//$("#project-description").html(result['summary']);
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette" , colorCallback);
+}
+
+function colorCallback(result){
+	console.log(result);
+	var colors = [result['hex']]; 
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+	//var help = '<a href="#" class="thumbnail">' +
+	//'<img src="' + result['image'] + '"class=detailsImage"></a>' +  
+//	'<h5>' + result['date'] + '</h5>' + '<p>' + result['summary'] + '</p>';
+//details#project"+ result['id']).html(help);
+	
+
 }
